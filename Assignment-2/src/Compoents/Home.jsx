8b367f172048da967f884f5d67037cbb3442e7be
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-12-28 21:10:38 -0700

    learned todo list in react

--- Assignment-2/src/Compoents/Home.jsx
+++ Assignment-2/src/Compoents/Home.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import NewTodo from "./NewTodo";
 import Todos from "./Todos";
 
+const dummyTodos = ["todo1", "todo2"];
+
 const Home = () => {
-  const todos = ["todo1", "todo2"];
+  const [todos, setTodos] = useState(dummyTodos);
+
+  const handleNewTodo = (newTodo) =>{
+    setTodos([...todos, newTodo])
+  }
+
   return (
     <div>
-      <NewTodo />
+      <NewTodo onTodo={handleNewTodo}/>
       <Todos todos={todos} />
     </div>
   );
--- Assignment-2/src/Compoents/NewTodo.jsx
+++ Assignment-2/src/Compoents/NewTodo.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
 
-const NewTodo = () => {
+const NewTodo = (props) => {
+
+  const [todo, setTodo] = useState("");
+  const handleInputChange = (e) =>{
+    setTodo(e.target.value);
+
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    props.onTodo(todo)
+  }
   return (
     
-    <form action="">
-        <label htmlFor="todo">New Todo</label>
-        <input type="text" name="" id="" />
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="todo">I am New Todo: </label>
+        <input type="text" name="todo" id="todo" value={todo} onChange={handleInputChange}/>
+        <button>Add Todo</button>
     </form>
   )
 }
