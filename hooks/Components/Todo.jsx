5f49820a7314d4b583e894e985207b6adb8ac6df
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-01-05 22:02:53 -0700

    finished todo-list

--- hooks/Components/Todo.jsx
+++ hooks/Components/Todo.jsx
@@ -5,7 +5,6 @@ const Todo = () => {
   const [list, setList] = useState([]);
 
   const captureItem = (event) => {
-  
     setItem(event.target.value);
   };
 
@@ -14,26 +13,39 @@ const Todo = () => {
     setList([...list]);
   };
 
+  const removeItem = (index) => {
+    list.splice(index, 1);
+    setList([...list]);
+  };
+
   return (
     <>
       <h1>Todo</h1>
       <table>
-        
-        {list.length !== 0? (
-            list.map((ele, index) => {
-                return(
-                    <tr  key={index}>
-                        <td style={{fontSize: "25px"}}> {ele} </td>
-                        <button style={{borderRadius: "10px", padding: "10px 15px", backgroundColor: "#B80000", cursor: "pointer"}}>Remove</button>
-                    </tr>
-                    
-
-                )
-            })
-        ):(
-            <tr>{"Nothing to show for now !!!"}</tr>
+        {list.length !== 0 ? (
+          list.map((ele, index) => {
+            return (
+              <tr key={index}>
+                <td style={{ fontSize: "25px" }}> {ele} </td>
+                <button
+                  onClick={() => {
+                    removeItem(index);
+                  }}
+                  style={{
+                    borderRadius: "10px",
+                    padding: "10px 15px",
+                    backgroundColor: "#B80000",
+                    cursor: "pointer",
+                  }}
+                >
+                  Remove
+                </button>
+              </tr>
+            );
+          })
+        ) : (
+          <tr>{"Nothing to show for now !!!"}</tr>
         )}
-       
       </table>
       <input
         onChange={captureItem}
