969b4932c55c0a6b8b9c124f7e36a74338e9ce9c
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-01-19 23:35:31 -0700

    state

--- Use-State/src/App.jsx
+++ Use-State/src/App.jsx
@@ -1,16 +1,19 @@
-import State from './State.jsx'
+import State from "./State.jsx";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
+  const counter = 10;
   return (
     <>
-     
-       <State />
-
+      <State />
+      <h1>React with Ivan {counter}</h1>
+      <h2>Counter value : {counter}</h2>
+      <button>Add value</button> {" "}
+      <button>Decrease value</button>
+      <p>footer: {counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
